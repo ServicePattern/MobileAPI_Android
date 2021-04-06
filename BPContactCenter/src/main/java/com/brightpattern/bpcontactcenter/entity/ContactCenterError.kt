5975ec3be0b6cb0374bcc23d2fea3c30760bcdc2
argiad <a.s.mkrtchyan@gmail.com>
2021-04-06 05:29:39 -0500

    validate the TenantURL

--- BPContactCenter/src/main/java/com/brightpattern/bpcontactcenter/entity/ContactCenterError.kt
+++ BPContactCenter/src/main/java/com/brightpattern/bpcontactcenter/entity/ContactCenterError.kt
@@ -4,6 +4,7 @@ import org.json.JSONException
 
 sealed class ContactCenterError(text: String) : Error(text) {
     data class FailedToBuildBaseURL(val text: String) : ContactCenterError(text)
+    data class FailedTenantURL(val text: String) : ContactCenterError(text)
     data class FailedToCodeJSON(val event: ContactCenterEvent, val jsonException: JSONException) : ContactCenterError("FailedToCodeJCON")
     data class FailedToCreateURLRequest(val volleyException: com.android.volley.VolleyError) : ContactCenterError("FailedToCreateURLRequest")
     data class BadStatusCode(val statusCode: Int, val volleyException: com.android.volley.VolleyError) : ContactCenterError("badStatusCode")
