da68459dc69208f64def6ee18ec663ec425cfa17
Artem Mkrtchyan <a.s.mkrtchyan@gmail.com>
2021-04-12 21:42:43 -0500

    Update ContactCenterVersion.kt

--- BPContactCenter/src/main/java/com/brightpattern/bpcontactcenter/model/ContactCenterVersion.kt
+++ BPContactCenter/src/main/java/com/brightpattern/bpcontactcenter/model/ContactCenterVersion.kt
@@ -4,8 +4,10 @@ import com.brightpattern.bpcontactcenter.entity.FieldName
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-/// Service status
-/// - Tag: ContactCenterServiceAvailability
+/**
+  * Service status
+  * ContactCenterServiceAvailability
+**/
 @Serializable
 data class ContactCenterVersion(
         @SerialName(FieldName.SERVER_VERSION)
