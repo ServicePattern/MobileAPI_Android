61c03539b3db7d33b3ad34a0363140edd6951ef8
Alexander Lobastov <58867146+ALobastov@users.noreply.github.com>
2021-06-28 12:12:35 -0700

    Fix notAavailable response

--- BPContactCenter/src/main/java/com/brightpattern/bpcontactcenter/model/ContactCenterServiceAvailability.kt
+++ BPContactCenter/src/main/java/com/brightpattern/bpcontactcenter/model/ContactCenterServiceAvailability.kt
@@ -10,7 +10,7 @@ enum class ContactCenterServiceChatAvailability {
     @SerialName("available")
     Available,
 
-    @SerialName("unavailable")
+    @SerialName("notAvailable")
     Unavailable
 }
 
