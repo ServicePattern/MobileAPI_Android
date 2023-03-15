e59f227c2f792c260ce6050385f634481bed5700
Artem Mkrtchyan <a.s.mkrtchyan@gmail.com>
2023-03-15 01:32:52 -0500

    Call Activity layout

---
+++ chatdemo/src/main/java/com/brightpattern/CallActivity.kt
@@ -0,0 +1,18 @@
+package com.brightpattern
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.brightpattern.chatdemo.R
+
+class CallActivity: AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_call)
+
+
+    }
+
+}
